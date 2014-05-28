cb1610459974657cd1c3cedf9f2f957eecc548de
littlell <littlell@126.com>
2014-05-28 11:16:07 +0800

    adding capi 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,53 +1,64 @@
-require "bundler/capistrano"
+require 'bundler/capistrano'
+set :application, "china_where"
+set :repository, 'git@github.com:chinawhere/chinawhere.git'
+set :deploy_to, "/tmp/#{application}"
+
+set :user, "root"
+
+server "106.187.91.138", :web, :app, :db, primary: true
+
+# set :default_environment, {
+#   'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+# }
+
+
+# require "bundler/capistrano"
 
 # server "106.187.91.138", :web, :app, :db, primary: true
 
-server "localhost", :web, :app, :db, primary: true
-
-set :application, "china_where"
-set :user, "root"
-# set :deploy_to, "/home/#{user}/apps/#{application}"
-set :deploy_to, "/tmp/#{application}"
+# set :application, "china_where"
+# set :user, "root"
+# # set :deploy_to, "/home/#{user}/apps/#{application}"
+# set :deploy_to, "/tmp/#{application}"
 # set :deploy_via, :remote_cache
-# set :use_sudo, false
+# # set :use_sudo, false
 
-set :scm, "git"
-set :repository, "git@github.com:chinawhere/chinawhere.git"
-set :branch, "master"
+# set :scm, "git"
+# set :repository, "git@github.com:chinawhere/chinawhere.git"
+# set :branch, "master"
 
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
+# default_run_options[:pty] = true
+# ssh_options[:forward_agent] = true
 
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
+# after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
-namespace :deploy do
-  %w[run stop].each do |command|
-    desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "rake unicorn:#{command}"
-    end
-  end
+# namespace :deploy do
+  # %w[run stop].each do |command|
+  #   desc "#{command} unicorn server"
+  #   task command, roles: :app, except: {no_release: true} do
+  #     run "rake unicorn:#{command}"
+  #   end
+  # end
 
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    puts "Now edit the config files in #{shared_path}."
-  end
-  after "deploy:setup", "deploy:setup_config"
-  task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-  after "deploy:finalize_update", "deploy:symlink_config"
-  p '666'
-  desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
-      puts "Run `git push` to sync changes."
-      exit
-    end
-  end
-  before "deploy", "deploy:check_revision"
-end
+  # task :setup_config, roles: :app do
+  #   sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+  #   sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+  #   run "mkdir -p #{shared_path}/config"
+  #   put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+  #   puts "Now edit the config files in #{shared_path}."
+  # end
+  # after "deploy:setup", "deploy:setup_config"
+  # task :symlink_config, roles: :app do
+  #   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  # end
+  # after "deploy:finalize_update", "deploy:symlink_config"
+  # desc "Make sure local git is in sync with remote."
+  # task :check_revision, roles: :web do
+  #   unless `git rev-parse HEAD` == `git rev-parse origin/master`
+  #     puts "WARNING: HEAD is not the same as origin/master"
+  #     puts "Run `git push` to sync changes."
+  #     exit
+  #   end
+  # end
+  # before "deploy", "deploy:check_revision"
+# end
