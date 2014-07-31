c25ed08f5755448779d728a62bca373cfa93a5b0
lizhenjiang <hellolzj123@163.com>
2014-07-31 21:49:37 +0800

    rescue nil

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,7 +6,7 @@ class Admin::ApplicationController < ApplicationController
   layout 'admin'
 
   def admin_access_denied
-    @current_staffer = User.find(session[:staffer_id])
+    @current_staffer = User.find(session[:staffer_id]) rescue nil
     redirect_to admin_login_path and return unless @current_staffer 
     redirect_to root_path unless @current_staffer.has_role? :admin
   end
