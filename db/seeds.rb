b909089e10c9a689e2abc293a402a5026e0e447b
tang.l1 <tang.l1@yougou.com>
2014-08-01 16:40:05 +0800

    register

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ User.where( name:     '唐玲',
             mobile:   '18701175667',
             position: 'ruby工程师',
             age:      26,
-            qq:       '361771529').first_or_create
+            qq:       '361771529').first_or_create.add_role :admin
 
  [{:name=>"运动旅行", :status=>"event"},{:name=>"交友聚会", :status=>"event"},{:name=>"教育培训", :status=>"event"},
   {:name=>"沙龙讲座", :status=>"event"},{:name=>"艺术文化", :status=>"event"},{:name=>"消费体验", :status=>"event"},
