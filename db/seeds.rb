3963cf7fe19d5635d2af85b5e6e244b560db3d21
lizhenjiang <hellolzj123@163.com>
2014-08-04 11:22:06 +0800

    aaa

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,9 @@ User.where( name:     '唐玲',
             age:      26,
             qq:       '361771529').first_or_create
 
+user = User.first
+user.add_role? :admin
+
  [{:name=>"运动旅行", :status=>"event"},{:name=>"交友聚会", :status=>"event"},{:name=>"教育培训", :status=>"event"},
   {:name=>"沙龙讲座", :status=>"event"},{:name=>"艺术文化", :status=>"event"},{:name=>"消费体验", :status=>"event"},
   {:name=>"会议展览", :status=>"event"},{:name=>"社会公益", :status=>"event"},{:name=>"其他", :status=>"event"}].each do |k|
