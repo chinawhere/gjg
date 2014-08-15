070046a2947b8f56141cebb6ae98442981731571
littlell <littlell@126.com>
2014-08-15 15:36:33 +0800

    2014-08-15 15:36:33 +0800

--- config/deploy.rb
+++ config/deploy.rb
@@ -72,6 +72,7 @@ task :deploy => :environment do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
+    invoke :clean_assets
     invoke :'rails:assets_precompile'
 
     to :launch do
