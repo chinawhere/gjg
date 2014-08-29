fd4916b5693f1253fbedd93ac64f3dbd4ac43381
tang.l1 <tang.l1@yougou.com>
2014-08-29 13:52:48 +0800

    destroy comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,18 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   scope :p_comments, where(:p_comment_id => nil)
 
+  def p_comment_user_id
+    self.class.find(self.p_comment_id).try(:user_id) rescue nil
+  end
+
+  before_save do
+    self.p_user_id = self.p_comment_user_id
+  end
+
+  after_destroy do
+    self.r_comments.destroy_all
+  end
+
   def r_comments
     self.class.where(:p_comment_id => self.id)
   end
