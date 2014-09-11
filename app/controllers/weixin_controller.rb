8db3a516949658facfe63a759276dcf84dd390a9
littlell <littlell@126.com>
2014-09-11 14:29:52 +0800

    2014-09-11 14:29:52 +0800

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -4,5 +4,16 @@ class WeixinController < ApplicationController
   end
 
   def code
+  	if params[:code].present?
+  		res = Faraday.post "https://api.weixin.qq.com/sns/oauth2/access_token", {
+  			:appid => "wx7cdd37389daaf639",
+  			:secret => "b8caf1ccbf6e520c6801c071052ba813",
+  			:code => params[:code],
+  			:grant_type => "authorization_code"
+  		}
+  		render :text => res.body
+  	else
+  		redirect_to "http://www.baidu.com"
+  	end
   end
 end
