2cb0b7630f7a80f53eac4310e3fe70ea636eaa57
littlell <littlell@126.com>
2014-09-11 14:43:23 +0800

    2014-09-11 14:43:23 +0800

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -4,17 +4,15 @@ class WeixinController < ApplicationController
   end
 
   def code
-  	if params[:code].present?
-  		res = Faraday.post "https://api.weixin.qq.com/sns/oauth2/access_token", {
-  			:appid => "wx7cdd37389daaf639",
-  			:secret => "b8caf1ccbf6e520c6801c071052ba813",
-  			:code => params[:code],
-  			:grant_type => "authorization_code"
-  		}
-  		parsed_json = JSON.parse res.body
-  		render :text => parsed_json['access_token']
-  	else
-  		redirect_to "http://www.baidu.com"
-  	end
+		res = Faraday.post "https://api.weixin.qq.com/sns/oauth2/access_token", {
+			:appid => "wx7cdd37389daaf639",
+			:secret => "b8caf1ccbf6e520c6801c071052ba813",
+			:code => params[:code],
+			:grant_type => "authorization_code"
+		}
+		parsed_json = JSON.parse res.body
+		# render :text => parsed_json['access_token']
+		session[:weixin_id] = parsed_json[:openid]
+		render :text => session[:weixin_id]
   end
 end
