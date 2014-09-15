8347f8c5a6287ff2937b29f7711a101924500eea
littlell <littlell@126.com>
2014-09-15 10:14:28 +0800

    2014-09-15 10:14:28 +0800

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   rolify
   # validates :email, :presence => { :message => "请填写联系邮箱" }
   # validates :password, :confirmation => true, :presence => { :message => '请填写密码'}
-  validates :name, presence: {message: '输入不能为空'}, uniqueness: {message: '名字已被占用'}
+  # validates :name, presence: {message: '输入不能为空'}, uniqueness: {message: '名字已被占用'}
   validates :email, presence: {message: '输入不能为空'}, uniqueness: {message: '邮箱已被占用'}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: '邮箱格式不正确' }
   validates :password, presence: {message: '输入不能为空'}, length: { minimum: 2, maximum: 16, message: '请设置2-16位英文字母、数字、符号密码' }, confirmation: {message: '密码输入不一致'}
 
