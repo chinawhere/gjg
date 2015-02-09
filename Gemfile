a177c385c4d959cdc9436ad7aedaa8c8da4c2f27
lizhenjiang <hellolzj123@163.com>
2015-02-09 17:21:26 +0800

    aaa

--- Gemfile
+++ Gemfile
@@ -42,9 +42,9 @@ gem 'sqlite3'
 #   gem 'sqlite3'
 # end
 
-group :production do
-  gem 'pg'
-end
+# group :production do
+#   gem 'pg'
+# end
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,16 @@
-# require 'mina/puma'
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+require 'mina/rbenv'
 
-set :user, "root"
-set :domain, "123.56.102.78"
-
-set :deploy_to, "/opt/project/chinawhere"
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+set :user , 'root'
+set :domain, '123.56.102.78'
+set :deploy_to, '/opt/project/chinawhere'
 set :repository, '/opt/project/chinawhere.git'
 set :branch, 'master'
 
@@ -15,53 +18,42 @@ set :branch, 'master'
 # They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['config/database.yml', 'log']
 
-desc "Push repository to the remote server"
-task :before_clone do
-  system "git add . -A"
-  system "git commit -m '#{Time.now.to_s}'"
-  system "git push chinawhere #{branch}"
-end
-
-desc "run db:seed"
-task :db_seed do
-  queue 'bundle exec rake db:seed'
-end
-
 task :environment do
   invoke :'rbenv:load'
 end
 
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
 task :setup => :environment do
-  queue! %[mkdir -p "#{deploy_to}/shared/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
 
-  queue! %[mkdir -p "#{deploy_to}/shared/config"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
 
-  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
+  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml'."]
 end
 
 desc "Deploys the current version to the server."
 task :deploy => :environment do
   deploy do
-    invoke :before_clone
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
-    queue! "RAILS_ENV=production bundle"
+    invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
 
     to :launch do
-      queue! "RAILS_ENV=production bundle exec rake restart_puma"
+      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+      # queue! "RAILS_ENV=production bundle exec rake restart_puma"
+      queue! "bundle exec rake restart_unicorn"
+      queue! "ln -sv #{deploy_to}/#{shared_path}/public/spree #{deploy_to}/#{current_path}/public/spree"
     end
   end
 end
-
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
 
