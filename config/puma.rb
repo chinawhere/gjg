f27ceade80dff7ad3ff1a59a5308c5da81120947
lizhenjiang <hellolzj123@163.com>
2015-02-11 17:05:40 +0800

    aa

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,7 @@ basedir = "/opt/project/chinawhere"
 daemonize true
 threads 2,16
 
-bind  "unix:///opt/project/chinawhere/tmp/puma.sock"
+bind  "unix:///tmp/puma.sock"
 pidfile  "#{basedir}/tmp/puma.pid"
 state_path "#{basedir}/tmp/puma.state"
 preload_app!
