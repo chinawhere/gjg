97f84eb7518d9289d1b00115ab688ce0c2c4e085
littlell <littlell@126.com>
2015-03-18 15:53:07 +0800

    change syntax to rails 4

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,9 @@
 # coding: utf-8
 class Comment < ActiveRecord::Base
-  # attr_accessible :title, :body
   belongs_to :commentable, polymorphic: true
 
   belongs_to :user
-  scope :p_comments, where(:p_comment_id => nil)
+  scope :p_comments ,->{ where(:p_comment_id => nil) }
 
   def p_comment_user_id
     self.class.find(self.p_comment_id).try(:user_id) rescue nil
--- app/models/question_classify.rb
+++ app/models/question_classify.rb
@@ -1,4 +1,4 @@
 class QuestionClassify < ActiveRecord::Base
   attr_accessible :name, :parent_id
-  scope :top_category, where(:parent_id => nil)
+  scope :top_category, ->{where(:parent_id => nil)}
 end
