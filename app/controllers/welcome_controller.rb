b2d7804faddbe08f96b7d4c21316b9420a6c88b8
littlell <littlell@126.com>
2015-03-19 16:04:13 +0800

    2015-03-19 16:04:13 +0800

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,8 @@
 class WelcomeController < ApplicationController
 
   def index
-    render text: request.ip and return
+    res = Faraday.get %{http://ip.taobao.com/service/getIpInfo.php?ip=#{request.ip}}
+    render text: res.inspect and return
   	@events = Event.limit(10)
   end
 
