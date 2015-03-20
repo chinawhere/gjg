c544fe329cb4356bb534714391850413fe9bea18
littlell <littlell@126.com>
2015-03-20 10:56:47 +0800

    修改登陆和注册

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,10 +16,11 @@ class SessionsController < ApplicationController
   end
 
   def register
+    @user ||= User.new
   end
 
   def sign_up
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
@@ -33,5 +34,11 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     @current_user = nil
     redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
