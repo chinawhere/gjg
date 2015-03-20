7196aef244118a49c0a69fde64eadbe8006bdc0b
lizhenjiang <hellolzj123@163.com>
2015-03-20 16:18:07 +0800

    layout

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,7 +3,8 @@ class CitiesController < ApplicationController
   def pick
     @city = City.find(params[:id])
     cookies.signed[:city_id] = @city.id
-    redirect_to root_path
+    # redirect_to root_path 
+    redirect_to request.referer
   end
 
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,6 @@
 class EventsController < ApplicationController
   before_filter :require_login, except: [:index, :show, :photos]
   before_filter :load_event, only: [:show, :update, :edit, :destroy, :uploader, :photos]
-  layout 'event'
   def index
     @events = Event.search(params).paginate(page: params[:page] || 1, per_page: params[:per_page] || 10)
     photo_ids = @events.map(&:photos_path).compact.join(',').split(',') rescue []
