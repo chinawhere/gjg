69c891cd62a7b66d4b09596d8f58b63172aaf4ec
littlell <littlell@126.com>
2015-03-20 13:49:42 +0800

    2015-03-20 13:49:42 +0800

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module ChinaWhere
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.secret_key_base = 'blipblapblup'
+    # config.secret_key_base = 'blipblapblup'
   end
 end
