e74472d7ad2bf82c5b31f0bcf67343f72339bc37
lizhenjiang <hellolzj123@163.com>
2015-05-12 11:36:23 +0800

    aaa

--- app/controllers/trade_controller.rb
+++ app/controllers/trade_controller.rb
@@ -1,9 +1,19 @@
 class TradeController < ApplicationController
+	skip_before_filter :verify_authenticity_token
+	before_filter :check_weixin_legality
   def index
   	render "index", :formats => :xml
   end
 
   def call_back
-  	render :text => params
+  	# render :text => params
+  	render :text => params[:echostr]
+  end
+
+  private
+  # 根据参数校验请求是否合法，如果非法返回错误页面
+  def check_weixin_legality
+    array = [Rails.configuration.weixin_token, params[:timestamp], params[:nonce]].sort
+    render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)  	
   end
 end
