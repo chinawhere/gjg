2564c998e7cbc2cae4418b877d7b65f5b50f4b42
lizhenjiang <hellolzj123@163.com>
2015-06-08 16:56:35 +0800

    2015-06-08 16:56:35 +0800

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,7 +53,7 @@ task :deploy => :environment do
     invoke :submit_git
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
-    # invoke :'bundle:install'
+    invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
