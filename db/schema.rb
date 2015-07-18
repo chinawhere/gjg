43f3d93d33f1bc0d1166ff9fe60b2edad8a191f0
夏雷 <littlell@126.com>
2015-07-19 02:20:30 +0800

    2015-07-19 02:20:30 +0800

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150703061047) do
+ActiveRecord::Schema.define(version: 20150718181838) do
 
   create_table "applies", force: :cascade do |t|
     t.integer  "user_id"
@@ -38,22 +38,6 @@ ActiveRecord::Schema.define(version: 20150703061047) do
   end
 
   add_index "cities", ["code"], name: "index_cities_on_code", unique: true
-
-  create_table "ckeditor_assets", force: :cascade do |t|
-    t.string   "data_file_name",               null: false
-    t.string   "data_content_type"
-    t.integer  "data_file_size"
-    t.integer  "assetable_id"
-    t.string   "assetable_type",    limit: 30
-    t.string   "type",              limit: 30
-    t.integer  "width"
-    t.integer  "height"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
-  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
