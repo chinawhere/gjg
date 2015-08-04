27489236956409ef2e0a4bdd3c6b0919a78c3ec1
夏雷 <littlell@126.com>
2015-08-04 15:26:46 +0800

    2015-08-04 15:26:46 +0800

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,5 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
 //= require kindeditor
 //= require_directory .
