c957e2bbcfead0007e3b5dd91465168d7431cb16
lzj <hellolzj123@163.com>
2016-10-17 17:21:52 +0800

    2016-10-17 17:21:51 +0800

--- app/controllers/taxons_controller.rb
+++ app/controllers/taxons_controller.rb
@@ -1,14 +1,11 @@
 class TaxonsController < ApplicationController
 	def list
-		puts params
 		@taxon = Taxon.find(params[:id]) if params[:id].present?
-	    if @taxon
-	    	puts "a" * 100
-	      @events = @taxon.events.paginate(page: params[:page] || 1, per_page: params[:per_page] || 10)
-	    else
-	    	puts "b" * 100
-	      @events = Event.all.paginate(page: params[:page] || 1, per_page: params[:per_page] || 10)
-	    end
+    if @taxon.parent.present?
+      @events = @taxon.events.paginate(page: params[:page] || 1, per_page: params[:per_page] || 10)
+    else
+      @events = Event.where(category_id: @taxon.sub_menus.try(:id)).paginate(page: params[:page] || 1, per_page: params[:per_page] || 10)
+    end
 		render template: 'events/index'
 	end
 end
